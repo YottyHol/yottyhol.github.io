ac24fd0d74e63ada6754fc301f1e49a025269b0e
olive <evilo99@hotmail.com>
2024-10-28 21:47:07 +0000

    deploy test

---
+++ yotty-pages/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/yottyhol.github.io', // Replace with your GitHub repo name
+  assetPrefix: '/yottyhol.github.io/', // Also replace with your repo name
+  // Other config options can go here
+};
+
+export default nextConfig;
