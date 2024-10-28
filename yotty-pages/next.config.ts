4bc4cc1e4d6ef1ee061dfc7f7627b8961f7db24e
olive <evilo99@hotmail.com>
2024-10-28 22:20:01 +0000

    deploy

--- yotty-pages/next.config.ts
+++ yotty-pages/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'development' ? '' : '/yottyhol.github.io', // Only use basePath in production
-  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/yottyhol.github.io/', // Adjust for production
+  basePath: '/yottyhol.github.io', // Only use basePath in production
+  assetPrefix: '/yottyhol.github.io/', // Adjust for production
   // Other config options can go here
 };
 
